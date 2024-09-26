2acf4f0d637e3183c6c98792df45643c9e15a8fd
khlee <panpan68@naver.com>
2024-09-26 13:44:55 +0900

    Dispatchers and threads

---
+++ src/main/kotlin/part5/DispathersAndThreads.kt
@@ -0,0 +1,40 @@
+package part5
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+
+/**
+ * @Description : DispathersAndThreads.java
+ * @author      : heon
+ * @since       : 2024-09-26
+ *
+ * <pre>
+ *
+ * << 개정이력(Modification Information) >>
+ *
+ * ===========================================================
+ * 수정인          수정자      수정내용
+ * ----------    ----------    --------------------
+ * 2024-09-26       heon         최초 생성
+ *
+ * <pre>
+ */
+fun main() = runBlocking<Unit> {
+    launch {
+        println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Unconfined) {
+        // not confined -- will work with main thread
+        println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(Dispatchers.Default) {
+        // will get dispatched to DefaultDispatcher
+        println("Default               : I'm working in thread ${Thread.currentThread().name}")
+    }
+    launch(newSingleThreadContext("MyOwnThread")) {
+        // will get its own new thread
+        println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+    }
+}
