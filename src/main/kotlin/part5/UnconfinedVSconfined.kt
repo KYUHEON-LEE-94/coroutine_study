4004c31bc85b8ecbad7281609b0a5a606d7f153a
khlee <panpan68@naver.com>
2024-09-26 13:54:59 +0900

    스레드 한정 디스패처와 비-한정 디스패처

---
+++ src/main/kotlin/part5/UnconfinedVSconfined.kt
@@ -0,0 +1,37 @@
+package part5
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+/**
+ * @Description : UnconfinedVSconfined.java
+ * @author      : heon
+ * @since       : 2024-09-26
+ *
+ * <pre>
+ *
+ * << 개정이력(Modification Information) >>
+ *
+ * ===========================================================
+ * 수정인          수정자      수정내용
+ * ----------    ----------    --------------------
+ * 2024-09-26       heon         최초 생성
+ *
+ * <pre>
+ */
+fun main() = runBlocking <Unit> {
+    launch (Dispatchers.Unconfined) {
+        // not confined -- will work with main thread
+        println("Unconfined      : I'm working in thread ${Thread.currentThread().name}")
+        delay(500)
+        println("Unconfined      : After delay in thread ${Thread.currentThread().name}")
+    }
+    launch {
+        // context of the parent, main runBlocking coroutine
+        println("main runBlocking: I'm working in thread ${Thread.currentThread().name}")
+        delay(1000)
+        println("main runBlocking: After delay in thread ${Thread.currentThread().name}")
+    }
+}
